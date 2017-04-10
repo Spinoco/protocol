0aaab2eb6b7c87219d39918becd5d7aa81feb3fd
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2017-04-10 15:21:58 +0200

    Added helpers for selecting http headers

--- http/src/main/scala/spinoco/protocol/http/HttpRequestHeader.scala
+++ http/src/main/scala/spinoco/protocol/http/HttpRequestHeader.scala
@@ -1,5 +1,6 @@
 package spinoco.protocol.http
 
+import shapeless.Typeable
 import spinoco.protocol.http.header.HttpHeader
 
 /**
@@ -17,4 +18,14 @@ sealed case class HttpRequestHeader(
   , headers: List[HttpHeader] = Nil
   , query: Uri.Query = Uri.Query.empty
   , version: HttpVersion.Value = HttpVersion.V1_1
-)
+) { self =>
+
+  /** gets first http header satisfying type `H` constrain **/
+  def firstHeader[H <: HttpHeader](implicit T: Typeable[H]): Option[H] =
+    self.headers.collectFirst(Function.unlift(T.cast))
+
+  /** select all headers of type `H` **/
+  def selectHeaders[H <: HttpHeader](implicit T: Typeable[H]): Seq[H] =
+    self.headers.collect(Function.unlift(T.cast))
+
+}
