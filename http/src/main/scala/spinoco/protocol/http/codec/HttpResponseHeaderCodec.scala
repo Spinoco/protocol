8c27ee1283ca679d03131336e4b7d9558071f496
Adam Chlupacek <adam@chlupacek.cz>
2018-02-02 17:11:20 +0100

    We can now read http resposes that are missing status code description.

--- http/src/main/scala/spinoco/protocol/http/codec/HttpResponseHeaderCodec.scala
+++ http/src/main/scala/spinoco/protocol/http/codec/HttpResponseHeaderCodec.scala
@@ -18,8 +18,13 @@ object HttpResponseHeaderCodec {
     val headerLineCodec: Codec[HttpVersion.Value :: HttpStatusCode :: String :: HNil] = {
 
       (bytesUntil(_ != ' ').codedAs(HttpVersion.codec) <~ whitespace()) ::
-      (bytesUntil(_ != ' ').codedAs(HttpStatusCode.codec) <~ whitespace()) ::
-      utf8String
+      choice(
+        (bytesUntil(_ != ' ').codedAs(HttpStatusCode.codec) <~ whitespace()) :: utf8String
+        , HttpStatusCode.codec.xmap[HttpStatusCode :: String :: HNil](
+          code => code :: code.description :: HNil
+          , { case code :: _ :: HNil => code }
+        )
+      )
     }
 
     parametrizedN(crlf, crlf, "Response" | headerLineCodec, "Headers" | headerCodec).xmap[HttpResponseHeader] (
